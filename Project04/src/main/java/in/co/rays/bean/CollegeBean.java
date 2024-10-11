7f2f43146afda0e1e3db7588e6414ef480be956e
avnishupadhyay2907 <166211443+avnishupadhyay2907@users.noreply.github.com>
2024-10-11 19:10:35 +0530

    Add files via upload

---
+++ Project04/src/main/java/in/co/rays/bean/CollegeBean.java
@@ -0,0 +1,41 @@
+package in.co.rays.bean;
+
+public class CollegeBean extends BaseBean {
+
+	private String name;
+	private String address;
+	private String state;
+	private String city;
+	private String phone_no;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPhone_no() {
+		return phone_no;
+	}
+	public void setPhone_no(String phone_no) {
+		this.phone_no = phone_no;
+	}
+
+}
---
+++ Project04/src/main/java/in/co/rays/bean/StudentBean.java
@@ -0,0 +1,64 @@
+package in.co.rays.bean;
+
+import java.util.Date;
+
+public class StudentBean extends BaseBean {
+
+	private String first_name;
+	private String last_name;
+	private Date dob;
+	private String gender;
+	private String mobile_no;
+	private String email;
+	private Long college_id;
+	private String college_name;
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMobile_no() {
+		return mobile_no;
+	}
+	public void setMobile_no(String mobile_no) {
+		this.mobile_no = mobile_no;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getCollege_id() {
+		return college_id;
+	}
+	public void setCollege_id(Long college_id) {
+		this.college_id = college_id;
+	}
+	public String getCollege_name() {
+		return college_name;
+	}
+	public void setCollege_name(String college_name) {
+		this.college_name = college_name;
+	}
+	
+}
